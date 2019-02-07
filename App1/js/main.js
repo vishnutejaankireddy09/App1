939ba562b63f1dce28590e46dcc6da1b79eb83e6
visualstudio <visualstudio@230-PC33>
2019-02-07 12:03:14 -0500

    Final Commit

---
+++ App1/js/main.js
@@ -0,0 +1,17 @@
+﻿// Your code here!
+function myFunction(a, b) {
+    var a = 5;
+    var b = 5;
+    return a + b; 
+}
+
+function arrays(){
+    var pens;
+    pens = ["red", "orange", "green", "blue", "purple"];
+    console.log(pens);
+    console.log(pens[0]);
+    for (i = 0; i <= pens.length; i++){
+        alert(pens[i]);
+    }
+    alert("Length of array is: " +pens.length);
+}
